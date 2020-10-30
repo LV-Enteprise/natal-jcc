85d3c29157611c15399f56e46e55881fc12b155e
LEONARDO ALVES <leonardo.alves17@fatec.sp.gov.br>
2020-10-30 20:19:35 -0300

    Feat: Mudança no arquivo css e Home.js

--- src/front/src/pages/Home.js
+++ src/front/src/pages/Home.js
@@ -3,27 +3,43 @@ import React from 'react'
 function Home() {
   return (
     <div className='home'>
-      <main>
-
-        <h1>
-          Campanha de Natal JCC
+      <h1>
+        Campanha de Natal JCC
       </h1>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+        <div className='content'>
+          <p>
+            Ajudando as pessoas a terem um Natal com mais esperança e amor!
+        </p>
+          <table className="table table-hover">
+            <thead>
+              <tr colspan="1" className="tabletitle">
+                <th> Famílias Cadastradas </th>
+                <a href="/families">
+                  <strong>
+                    Cadastre uma família
+                </strong>
+                </a>
+              </tr>
+              <tr className="linhas">
+                <th className="lin1" scope="col">Família</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="registro">
+                <th className="reg1" scope="row">Famíla A</th>
+                <a className="edit" href="/families/edit">
+                  <strong>
+                    Editar
+                </strong>
+                </a>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    </div>
 
-        <p>
-          Ajudando as pessoas a terem um Natal com mais esperança e amor!
-          </p>
-        <a href="/families">
-          <strong>
-            Cadastre uma família.
-            </strong>
-        </a>
-        <a href="/kids">
-          <strong>
-            Cadastre uma Criança.
-            </strong>
-        </a>
-      </main>
-    </div >
   )
 }
 
